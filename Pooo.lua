93c9e668a0c71814be02ebb22e09ef23ea98a507
Truongthanhden23023 <137466163+Truongthanhden23023@users.noreply.github.com>
2025-08-31 08:38:35 +0700

    Create Pooo.lua

---
+++ Pooo.lua
@@ -0,0 +1,10 @@
+local library = loadstring(game:HttpGet("https://raw.githubusercontent.com/SixZensED/Backups/refs/heads/main/Xelora%20Lib%202.lua"))()
+
+local main = library:CreateWindow({Title = "Pooo",Icon = 127435222710292})
+
+local tab = main:CreateTab({Title = "Main"})
+
+
+side:CreateButton({Title = "IY",callback = function()
+    print(loadstring(game:HttpGet('https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source'))(IY)
+end,})
