52fe5b51f6a0803d2da92067efb7d3fc808d231c
Truongthanhden23023 <137466163+Truongthanhden23023@users.noreply.github.com>
2025-08-31 08:49:11 +0700

    Update Pooo.lua

--- Pooo.lua
+++ Pooo.lua
@@ -1,10 +1,17 @@
 local library = loadstring(game:HttpGet("https://raw.githubusercontent.com/SixZensED/Backups/refs/heads/main/Xelora%20Lib%202.lua"))()
 
-local main = library:CreateWindow({Title = "Pooo",Icon = 127435222710292})
+local main = library:CreateWindow({Title = "Poooooo",Icon = 127435222710292})
 
-local tab = main:CreateTab({Title = "Main"})
+local tab = main:CreateTab({Title = "Tabs Main})
 
+
+
+local side = tab:CreateSection({Title = "Gay?",Side = "l"})
 
 side:CreateButton({Title = "IY",callback = function()
-    print(loadstring(game:HttpGet('https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source'))(IY)
+    print(loadstring(game:HttpGet('https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source'))()
+end,})
+
+side:CreateButton({Title = "Button ???",callback = function()
+    print(math.random(1,999))
 end,})
